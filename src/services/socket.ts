eb7786668cac5b8839910289f950aff14602e7bc
alanansari <ansarialan31@gmail.com>
2024-03-24 13:15:20 +0530

    socket player connection

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,7 +1,9 @@
 import { Server } from "socket.io";
+import { PrismaClient } from "@prisma/client";
 
 class SocketService {
     private _io: Server;
+    private prisma: PrismaClient;
     constructor() {
 
         console.log('Init Socket Service...');
@@ -13,14 +15,29 @@ class SocketService {
             }
         });
 
+        this.prisma = new PrismaClient();
     }
 
     public initListeners() {
         const io = this.io;
         console.log('Init Socket Listeners...');
 
-        io.on('connection', (socket) => {
-            console.log('New connection: ', socket.id);
+        io.on('connection', async (socket) => {
+            console.log('New connection:', socket.id);
+
+            const playerId = socket.handshake.query.playerId as string;
+
+            const player = await this.prisma.player.findUnique({
+                where: {
+                    id: playerId
+                }
+            });
+
+            if (!player) {
+                console.log('Player', playerId,'not found... \nDisconnecting Socket:', socket.id);
+                socket.disconnect();
+                return;
+            }
 
             socket.on('event:message',async ({message}:{message:string}) => {
                 console.log('New message: ', message);
